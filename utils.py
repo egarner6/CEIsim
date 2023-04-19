ffb1ee7ddbe3e60f5f0d3fe1f7815971d5e95f2b
Elizabeth <63018249+egarner6@users.noreply.github.com>
2023-04-19 15:09:50 -0400

    three environment options

--- utils.py
+++ utils.py
@@ -27,7 +27,7 @@ def genEnv(space, theta, f, rect_size, height, width):
     ]
 
     lower_wall = np.array([[0., 0.], R(theta) @ np.array([0.75*height, 0.])])
-    upper_wall = [[0., 0.], R(-theta) @ np.array([0.75*height,0.])]
+    upper_wall = [[0., 0.], R(-theta) @ np.array([0.75*height, 0.])]
     uw = [pt + np.array([0.8*width, height/2-(0.5*f*rect_size)]) for pt in upper_wall]
     lw = [pt + np.array([0.8*width, height/2+(0.5*f*rect_size)]) for pt in lower_wall]
     backwall = [uw[1], lw[1]]
